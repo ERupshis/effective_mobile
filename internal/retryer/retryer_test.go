2d612736c1040b8eb1f405577c300792ed256f5d
erups <e.rupshis@gmail.com>
2023-09-24 23:54:29 +0200

    added tests for retryer

--- internal/retryer/retryer_test.go
+++ internal/retryer/retryer_test.go
@@ -137,7 +137,89 @@ func TestRetryCallWithTimeout(t *testing.T) {
 		},
 	}
 	for _, tt := range tests {
-		_, _, err := RetryCallWithTimeout(tt.args.ctx, tt.args.log, tt.args.intervals, tt.args.repeatableErrors, tt.args.callback)
-		assert.Equal(t, tt.wantErr, err)
+		t.Run(tt.name, func(t *testing.T) {
+			_, _, err := RetryCallWithTimeout(tt.args.ctx, tt.args.log, tt.args.intervals, tt.args.repeatableErrors, tt.args.callback)
+			assert.Equal(t, tt.wantErr, err)
+		})
+	}
+}
+
+func TestRetryCallWithTimeoutErrorOnly(t *testing.T) {
+	log, _ := logger.CreateZapLogger("Info")
+
+	type args struct {
+		ctx              context.Context
+		log              logger.BaseLogger
+		intervals        []int
+		repeatableErrors []error
+		callback         func(context.Context) error
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "valid",
+			args: args{
+				ctx:              context.Background(),
+				log:              log,
+				intervals:        []int{1, 1, 1},
+				repeatableErrors: nil,
+				callback: func(ctx context.Context) error {
+					<-ctx.Done()
+					return errors.New(pgerrcode.ConnectionException)
+
+				},
+			},
+			wantErr: errors.New(pgerrcode.ConnectionException),
+		},
+		{
+			name: "valid with success",
+			args: args{
+				ctx:              context.Background(),
+				log:              log,
+				intervals:        nil,
+				repeatableErrors: nil,
+				callback: func(ctx context.Context) error {
+					return nil
+				},
+			},
+			wantErr: nil,
+		},
+		{
+			name: "valid should retry",
+			args: args{
+				ctx:              context.Background(),
+				log:              log,
+				intervals:        []int{1, 1, 1},
+				repeatableErrors: databaseErrorsToRetry,
+				callback: func(ctx context.Context) error {
+					<-ctx.Done()
+					return errors.New(pgerrcode.ConnectionException)
+				},
+			},
+			wantErr: errors.New(pgerrcode.ConnectionException),
+		},
+		{
+			name: "valid shouldn't retry",
+			args: args{
+				ctx:              context.Background(),
+				log:              log,
+				intervals:        []int{1, 1, 1},
+				repeatableErrors: databaseErrorsToRetry,
+				callback: func(ctx context.Context) error {
+					<-ctx.Done()
+					return errors.New("some error")
+				},
+			},
+			wantErr: errors.New("some error"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := RetryCallWithTimeoutErrorOnly(tt.args.ctx, tt.args.log, tt.args.intervals, tt.args.repeatableErrors, tt.args.callback)
+			assert.Equal(t, tt.wantErr, err)
+		})
 	}
 }
